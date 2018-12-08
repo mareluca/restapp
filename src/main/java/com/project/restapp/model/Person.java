07b66e3fd12beeb29df51d9e3a296766b7315e12
Marko Lucic <lucicmarko93@gmail.com>
2018-12-08 16:39:32 +0100

    Add something in model

--- src/main/java/com/project/restapp/model/Person.java
+++ src/main/java/com/project/restapp/model/Person.java
@@ -3,7 +3,13 @@ package com.project.restapp.model;
 import lombok.Data;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Data
 @Component
 public class Person {
+
+    private String name;
+    private String lastName;
+    private List<Key> keys;
 }
